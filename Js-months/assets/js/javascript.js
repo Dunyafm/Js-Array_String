58417a6a4c3580789c0003a45f8bc3fbeb36fb7c
unknown <Dunyafm@code.edu.az>
2022-01-29 12:16:21 -0800

    Js-months

---
+++ Js-months/assets/js/javascript.js
@@ -0,0 +1,30 @@
+// var month = new Array();
+
+// month['01'] = 'January' ;
+// month['02'] = 'February';
+// month['03'] = 'March' ;
+
+// var months = new Array(['01','Jan'],['02','Feb'],['03','Mar']);
+
+// let array =[01,02,03,0,05,06,07,08,09,10,11,12];
+// array.splice(array);
+// function getNumericMonth(months) {
+//     return (String(['January',
+//       'February',
+//       'March',
+//       'April',
+//       'May',
+//       'June',
+//       'July',
+//       'August',
+//       'September',
+//       'October',
+//       'November',
+//       'December'].indexOf(months) + array).padStart(2, '0'))
+//   }
+  
+//  console.log(getNumericMonth('February','March','April'));
+var month = new Array();
+var months = new Array(['Jan-01'],['Feb-02'],['Mar-03']);
+
+console.log(months);
